78b050b0924be27d3f48e0328f838ecfe3779f80
Richard Littauer <richard.littauer@gmail.com>
2015-11-29 10:50:40 -0500

    Added echo statements and .git check

--- dev/tools/hooks/setup_commit_msg_hook.sh
+++ dev/tools/hooks/setup_commit_msg_hook.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+if [ ! -d ".git" ]; then
+  # Control will enter here if $DIRECTORY exists.
+  echo "Cannot find .git folder!"
+  echo "Move this file to the root folder of a git repository and try again"
+else
+
 cat >.git/hooks/commit-msg <<'EOF'
 #!/bin/sh
 
 grep "^License:" "$1" || {
-	echo >>"$1"
-	echo "License: MIT" >>"$1"
-	echo "Signed-off-by: $(git config user.name) <$(git config user.email)>" >>"$1"
+  echo >>"$1"
+  echo "License: MIT" >>"$1"
+  echo "Signed-off-by: $(git config user.name) <$(git config user.email)>" >>"$1"
 }
 EOF
 chmod +x .git/hooks/commit-msg
+echo "Added commit-msg hook to this repo!"
 
+fi
